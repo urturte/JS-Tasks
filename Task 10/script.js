2954ba179cc848394213bec76e24d5034f7ce329
Urtė Nistirenko <urte.nistirenko@gmail.com>
2022-09-07 19:06:15 +0300

    tasks finished

--- Task 10/script.js
+++ Task 10/script.js
@@ -5,7 +5,7 @@ import { one, two, three, four, five } from "./modules/numbers/numbers.js";
 import { composition } from "./modules/math/composition.js";
 import { division } from "./modules/math/division.js";
 import { multiplication } from "./modules/math/multiplication.js";
-import { substraction } from "./modules/math/substraction.js";
+import { substraction } from "./modules/math/subtraction.js";
 
 let a = composition(one, four);
 let b = division(four, two);
